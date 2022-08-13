af20f00732bd390d5e170fcea5798f2ae1f8e19b
riteshray007 <ray.ritesh007@gmail.com>
2022-08-14 00:13:48 +0530

    final

--- src/components/favorites.js
+++ src/components/favorites.js
@@ -8,8 +8,16 @@ export class Favorites extends Component {
 
         this.state={
             moviedata:[],
-            genres : [  ]
+            genres : [],
+            currgenre : 'All Movies',
+            currentsearch : '',
         }
+    }
+    Handlecurrentgenre = (genre)=>{
+        this.setState({
+            currgenre : genre,
+        })
+
     }
 
     componentDidMount(){
@@ -51,7 +59,58 @@ export class Favorites extends Component {
         })
     }
 
+    handlesearchchange= (e)=>{
+        this.setState({
+            currentsearch : e.target.value ,
+        })
+    }
+    handledelete = (ID)=>{
+            let tempa = this.state.moviedata.filter((n)=> n.id != ID )
+
+            this.setState({
+                moviedata : [...tempa]
+            })
+
+            localStorage.setItem( 'movie-list' , JSON.stringify(tempa) )
+    }
+    handlepopularityinc = ()=>{
+        let temp = this.state.moviedata.sort(function(a,b){
+            return a.popularity - b.popularity
+        })
+
+        this.setState({
+            moviedata : [...temp],
+        })
+    }
+    handlepopularitydec= ()=>{
+        let temp = this.state.moviedata.sort(function(a, b){
+            return b.popularity - a.popularity
+        })
+
+        this.setState({
+            moviedata : [...temp]
+        })
+    }
+    handleratinginc=()=>{
+        let temp = this.state.moviedata.sort(function(a,b){
+            return a.vote_average - b.vote_average
+        })
+        this.setState({
+            moviedata : [...temp]
+        })
+    }
+    handleratingdec=()=>{
+        let temp = this.state.moviedata.sort(function(a,b){
+            return b.vote_average - a.vote_average
+        })
+        this.setState({
+            moviedata : [...temp]
+        })
+    }
+
     render() {
+
+        let filterarr = []
 
         let genreids = {
             28: "Action",
@@ -74,6 +133,26 @@ export class Favorites extends Component {
             10752: "War",
             37: "Western",
         }
+        if(this.state.currgenre == 'All Movies'){
+            filterarr = this.state.moviedata
+        }
+        else{
+            filterarr = this.state.moviedata.filter((data) => genreids[data.genre_ids[0]] == this.state.currgenre)
+        }
+        if(this.state.currentsearch === "" ){
+            
+        }
+        else{
+            filterarr = filterarr.filter((n) => {
+                let title = n.title.toLowerCase()
+                return title.includes(this.state.currentsearch.toLowerCase().trim())
+            })
+        }
+
+        
+
+
+
         
         // let moviedata = movies.results
         return (
@@ -81,18 +160,22 @@ export class Favorites extends Component {
                 <div className='row'>
                     <div className='col-3' >
                         <ul class="list-group  genre-selector " >
-                            {this.state.genres.map((n)=>{
-                                return(
+                            {this.state.genres.map((n)=>
+                                this.state.currgenre == n? 
+                                <li  
+                                    className="list-group-item genre-list " >{n}</li>
+                                    :
                                     <li  
-                                    className="list-group-item genre-list " aria-current="true">{n}</li>
-                                )
-                            })}
+                                    className="list-group-item genre-list-hover " 
+                                    onClick={()=>this.Handlecurrentgenre(n)} >{n}</li>
+                            )}
                             
                         </ul>
                     </div>
                     <div className='col-8 ' >
                         <div className='row ' >
-                            <input type='text' placeholder='Search'className='input-group-text col ' />
+                            <input type='text' placeholder='Search'className='input-group-text col '
+                             onChange={(e)=>this.setState({ currentsearch : e.target.value  })}  />
                             <input type='number' className='input-group-text  col numberinput' />
                         </div>
                         <div className='row' >
@@ -101,13 +184,14 @@ export class Favorites extends Component {
                                     <tr  >
                                         <th style={{width:'10%'}} ></th>
                                         <th scope="col" style={{ width: "40%" }} >Title</th>
-                                        <th scope="col">Genere</th>
-                                        <th scope="col">Popularity</th>
-                                        <th scope="col"  >Rating</th>
+                                        <th scope="col">Genere</th> 
+                                        <th scope="col"> <i class="fa-solid fa-sort-up" onClick={this.handlepopularitydec} ></i> Popularity <i class="fa-solid fa-sort-down" onClick={this.handlepopularityinc} ></i> </th>
+                                        <th scope="col"> <i class="fa-solid fa-sort-up " onClick={this.handleratingdec} ></i> Rating <i class="fa-solid fa-sort-down" onClick={this.handleratinginc} ></i> </th>
+                                        <th></th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {this.state.moviedata.map((n) => {
+                                    {filterarr.map((n) => {
                                         return (
                                             <tr>
                                                 <th><img style={{width:'5rem' , height : '4rem'} } src={`https://image.tmdb.org/t/p/original${n.backdrop_path}`} /></th>
@@ -115,6 +199,8 @@ export class Favorites extends Component {
                                                 <td>{genreids[n.genre_ids[0]]}</td>
                                                 <td>{n.popularity}</td>
                                                 <td>{n.vote_average}</td>
+                                                <td><button type="button" class="btn btn-outline-danger" style={{width : '4vw' , fontSize:'12px', padding:'5px'  }}  
+                                                onClick={()=>this.handledelete(n.id)} >Delete</button></td>
                                             </tr>
                                         )
                                     })}
